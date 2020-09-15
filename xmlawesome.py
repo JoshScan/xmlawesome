62efe5b4b0929683d51f30fa1ccc2c5d8157c6c8
JoshScan <41015445+JoshScan@users.noreply.github.com>
2020-09-15 09:33:51 -0400

    Update xmlawesome.py

--- xmlawesome.py
+++ xmlawesome.py
@@ -9,8 +9,16 @@ class xmlawesome:
 			dicts.append(item.__dict__)
 		
 		get_item_name = lambda x: item_name
-		xml = dicttoxml.dicttoxml(dicts, attr_type=False, custom_root=, item_func=get_item_name)
+		xml = dicttoxml.dicttoxml(dicts, attr_type=False, custom_root=root, item_func=get_item_name)
 		if pretty:
+			return parseString(xml).toprettyxml()
+			
+		return xml
+		
+	def objecttoxml(object, pretty=False):
+		name = type(object).__name__
+		xml = dicttoxml.dicttoxml(object.__dict__, attr_type=False, custom_root=name)
+		if pretty: 
 			return parseString(xml).toprettyxml()
 			
 		return xml
